a8d25bd1a397847f762a10479be56930c052763f
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-19 14:38:37 +0000

    fix the plot, invert x axis

--- plot_comps.py
+++ plot_comps.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import json
 import matplotlib.pyplot as plt
+import numpy as np
+
 from plot_images import plot_images
 
 plot_images()
@@ -10,7 +12,7 @@ def load_rms_mags_data():
     """
     Load RMS and magnitude data from JSON file.
     """
-    filename_1 = 'rms_vs_mag_comps.json'
+    filename_1 = 'files/rms_vs_mag_comps_0715.json'
     with open(filename_1, 'r') as file:
         data = json.load(file)
 
@@ -21,22 +23,19 @@ def plot_noise_model(data):
     """
     Plot RMS noise model on a given axis.
     """
-    good_rms = data['good_rms']
+    good_rms = np.array(data['good_rms']) * 1e6
     good_mags = data['good_mags']
-    bad_rms = data['bad_rms']
+    bad_rms = np.array(data['bad_rms']) * 1e6
     bad_mags = data['bad_mags']
-
-    dimmest_good_star_rms = min(good_rms)
-    y_limit_high = 3 * dimmest_good_star_rms  # Adjust multiplier as needed for clarity
-    y_limit_low = 0.01 * dimmest_good_star_rms  # Adjust multiplier as needed for clarity
 
     plt.figure()
     plt.scatter(good_mags, good_rms, color='black')
     plt.scatter(bad_mags, bad_rms, color='red')
     plt.xlabel('TESS Magnitude')
-    plt.ylabel('RMS')
-    plt.ylim(0.01, 0.06)  # Set y-axis limit based on dimmest good star RMS
-    plt.xlim(10.6, 13)
+    plt.ylabel('RMS (ppm)')
+    plt.ylim(0.01 * 1e6, 0.06 * 1e6)  # Set y-axis limit based on dimmest good star RMS
+
+    plt.gca().invert_xaxis()
     plt.ylim()
     # plt.title('RMS vs. Magnitude of Comparison Stars')
     plt.tight_layout()
