d5594822df7166bd7b78378a94c9a9a6ee41dc9a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-01-30 13:51:21 +0000

    script to detrend lc and to convert to csv file from json

--- detrend_lc.py
+++ detrend_lc.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import argparse
 from wotan import transit_mask, flatten
-from plot_images import plot_images, bin_time_flux_error, load_json
+from plot_images import plot_images, bin_time_flux_error, load_json, bin_by_time_interval
 
 
 def main(period, duration, T0, target_file, save_option):
@@ -16,19 +16,25 @@ def main(period, duration, T0, target_file, save_option):
     data = load_json(target_file)
 
     # Extract relevant data
+    # 1561 for CCD, 2000 for CMOS for WASP-30
     time = np.array(data['Time_BJD'])
     flux = np.array(data['Relative_Flux'])
     flux_err = np.array(data['Relative_Flux_err'])
+    print(f'Time length: {len(time)}, Flux length: {len(flux)}, Flux Error length: {len(flux_err)}')
 
-    # Bin the data
-    time_binned, flux_binned, fluxerr_binned = bin_time_flux_error(time, flux, flux_err, 30)
+    # Bin the data, for CMOS is 30 for 5 min and for CCD is 23 (10 + 3 seconds readout)
+    # time_binned, flux_binned, fluxerr_binned = bin_time_flux_error(time, flux, flux_err, 4)
+    # print(f'Binned Time length: {len(time_binned)}, Binned Flux length: {len(flux_binned)}, '
+    #       f'Binned Flux Error length: {len(fluxerr_binned)}')
+
+    time_binned, flux_binned, fluxerr_binned = time, flux, flux_err
 
     # Generate the transit mask
     mask = transit_mask(time=time_binned, period=period, duration=duration, T0=T0)
 
     # Apply Wotan flattening with transit masking
     flattened_flux, trend_flux = flatten(
-        time_binned, flux_binned, method='cosine',
+        time_binned, flux_binned, method='lowess',
         window_length=3 * duration, return_trend=True,
         robust=True, mask=mask
     )
@@ -39,13 +45,13 @@ def main(period, duration, T0, target_file, save_option):
     # Create a figure with side-by-side plots
     fig, axs = plt.subplots(1, 2, figsize=(10, 4), dpi=100, sharex=True, sharey=True)
     # Plot original light curve with trend
-    axs[0].plot(time_binned, flux_binned, 'ro', label='Original LC')
+    axs[0].plot(time_binned, flux_binned, 'r.', label='Original LC')
     axs[0].plot(time_binned, trend_flux, 'b-', label='Trend (Wotan)')
     axs[0].set_xlabel('Time (BJD)')
     axs[0].set_ylabel('Relative Flux')
 
     # Plot the detrended light curve with restored transit
-    axs[1].plot(time_binned, restored_flux, 'go', label='Detrended LC')
+    axs[1].plot(time_binned, restored_flux, 'g.', label='Detrended LC')
     axs[1].axhline(1, color='b', linestyle='--', label='Baseline')
     axs[1].set_xlabel('Time (BJD)')
 
@@ -57,7 +63,7 @@ def main(period, duration, T0, target_file, save_option):
         output_file_path = 'allesfitter/NGTS.csv'
         detrended_lc_df = pd.DataFrame({
             '# time': time_binned,
-            'flux': restored_flux,
+            'flux': flux_binned,
             'flux_err': fluxerr_binned
         })
         detrended_lc_df.to_csv(output_file_path, index=False)
@@ -83,6 +89,7 @@ if __name__ == "__main__":
     parser.add_argument('--duration', type=float, required=True, help="Transit duration in days")
     parser.add_argument('--T0', type=float, required=True, help="Mid-transit time (BJD)")
     parser.add_argument('--target_file', type=str, required=True, help="Path to the target JSON file")
+    # parser.add_argument('--bin_fac', type=int, default=30, help="Bin factor for time and flux")
     parser.add_argument('--save_option', type=str, default='no', choices=['yes', 'no'], help="Save CSV file (yes/no)")
 
     args = parser.parse_args()
