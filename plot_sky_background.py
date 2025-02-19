d49d5ccee2f9c0d74cd156583a5f63726d0470ee
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-02-19 11:45:56 +0000

    plots histogram disitrbution of all tic_id's sky background within the aperture on the image with the lowest airmass

--- plot_sky_background.py
+++ plot_sky_background.py
@@ -2,6 +2,8 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
+from networkx.algorithms.bipartite.basic import density
+
 from plot_images import plot_images
 
 plot_images()
@@ -16,16 +18,18 @@ def load_json(file_path):
 
 
 def main():
-    # Load the JSON data
-    sky_cmos_file = 'files/sky_flux_vs_temperature_CMOS_0705.json'
-    sky_ccd_file = 'files/sky_flux_vs_temperature_CCD_0705.json'
+    path = '/Users/u5500483/Download/'
+    sky_cmos_file = path + 'sky_flux_vs_temperature_CMOS_0911.json'
+    sky_ccd_file = path + 'sky_flux_vs_temperature_CCD_0911.json'
 
     print(f"Loading Sky CMOS data from {sky_cmos_file}...")
     sky_cmos_data = load_json(sky_cmos_file)
     print(f"Loading Sky CCD data from {sky_ccd_file}...")
     sky_ccd_data = load_json(sky_ccd_file)
 
-    # Extract converted flux values and normalize them by (400 * pi)
+    # Extract converted flux values and divide by the area in arcsec (r^2 * pi)
+    # CMOS: (Aperture = 5 px * 4 arcsec / px = 20 arcs)
+    # CCD: (Aperture = 4 px * 5 arcsec / px = 20 arcs)
     sky_flux_cmos = [entry["Converted_Flux"] / (400 * np.pi) for entry in sky_cmos_data]
     sky_flux_ccd = [entry["Converted_Flux"] / (400 * np.pi) for entry in sky_ccd_data]
 
@@ -36,21 +40,24 @@ def main():
     plt.figure()
     plt.hist(
         sky_flux_cmos,
-        bins=1000,
+        bins=100,
         color='blue',
         alpha=1,
-        label='Sky Flux (CMOS)'
+        label='Sky Flux (CMOS)',
+        density=True
     )
     plt.hist(
         sky_flux_ccd,
-        bins=1000,
+        bins=100,
         color='red',
         alpha=1,
-        label='Sky Flux (CCD)'
+        label='Sky Flux (CCD)',
+        density=True
     )
     plt.xlabel(r'Sky background ($\mathdefault{e^{-}s^{-1}\,arcsec^{-2}}$)')
     plt.ylabel('Frequency')
-    plt.xlim(0.47, 2)
+    # plt.xlim(0.47, 2)
+    # plt.xlim(1,9)
     plt.tight_layout()
     plt.savefig('sky_flux_histogram_0705.pdf', dpi=300)
     plt.show()
